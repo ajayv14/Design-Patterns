a088bcb6b4b230bccd39b997b19b23483821cb3e
ajay <ajay.v14@gmail.com>
2016-08-31 00:52:39 -0700

    Singleton pattern bug fixed

--- src/SingletonPattern/SingletonTestDemo.java
+++ src/SingletonPattern/SingletonTestDemo.java
@@ -4,4 +4,15 @@ package SingletonPattern;
  * Created by Ajay on 8/31/2016.
  */
 public class SingletonTestDemo {
+
+    public static void main(String args[]){
+
+       // Engine eng = new Engine();// will throw error
+
+        //Engine.getEngine(); // will return the only possible Engine object . i.e eng
+          Engine en = Engine.getEngine();
+               en.printEngineType();
+
+    }
+
 }
